de8f4162f268f0bfc66010e5705d1575c3b766cf
Zedon-Z <lrxzedon@gmail.com>
2025-07-13 20:42:11 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,31 +3,6 @@ from functools import wraps
 from telegram import Update
 from telegram.ext import CallbackContext
 from storage import authorized
-from telegram.ext import CallbackContext
-from telegram import Bot
-
-def countdown_alert(seconds_left, chat_id, bot):
-    def alert_fn():
-        emoji = "⏳" if seconds_left > 5 else "🚨"
-        bot.send_message(
-            chat_id=chat_id,
-            text=f"{emoji} *{seconds_left} seconds left before the game begins!*",
-            parse_mode='Markdown'
-        )
-    return alert_fn
-    
-def schedule_begin_game(chat_id, bot):
-    ctx = CallbackContext.from_bot(bot)
-    phases.begin_game(ctx, chat_id)
-    
-def create_context(bot: Bot) -> CallbackContext:
-    class DummyUpdate:
-        effective_chat = None
-        effective_user = None
-
-    context = CallbackContext.from_bot(bot)
-    context._update = DummyUpdate()
-    return context
     
 def group_allowed(func):
     @wraps(func)
