534780202bebc123ec50b45ca33b902977c9a60a
Zedon-Z <lrxzedon@gmail.com>
2025-07-19 14:25:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
-
+from telegram.ext import Updater
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+updater = Updater(token=os.getenv("TOKEN"), use_context=True)
+
+
 BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID", 1378500453))
 APP_URL = os.getenv("APP_URL")
