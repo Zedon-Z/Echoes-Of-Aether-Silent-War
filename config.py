80bb3a9d1db4c0e204f5d1a8bf4338edd59e0a77
Zedon-Z <lrxzedon@gmail.com>
2025-07-19 14:04:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("7990848107:AAHxCKvhqphMRLtMnWzakmmPLroSbfcjC6A")
 BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID", 1378500453))
 APP_URL = os.getenv("APP_URL")
